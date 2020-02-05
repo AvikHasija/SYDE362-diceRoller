e6beacb47c7049048f0b51587a556d1888e2ea96
Avik Hasija <avikhasija@gmail.com>
2020-02-05 16:10:48 -0500

    added buttons, stopped looping

--- diceRoller.ino
+++ diceRoller.ino
@@ -1,26 +1,42 @@
 #include <Servo.h> 
 int servoPin = 7;
+int buttonOnePin = 3;
+int buttonTwoPin = 2;
 Servo Servo1;
 
-int mode = 1;
+int mode = 0;
 
 void setup() {
+   Serial.begin(9600);
+   
    Servo1.attach(servoPin);
 
   // Reset to the starting point
   Servo1.write(90);
   delay(1000);
 
-  if (mode == 1) {
-   modeOne(); 
-  }
-  else if (mode == 2) {
-   modeTwo(); 
-  }
-   
+  pinMode(buttonOnePin, INPUT_PULLUP);
+  pinMode(buttonTwoPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonOnePin), switchToModeOne, FALLING);
+  attachInterrupt(digitalPinToInterrupt(buttonTwoPin), switchToModeTwo, RISING);
 }
 
 void loop(){
+  Serial.println(mode);
+  
+  if(mode == 1){
+    modeOne();
+  } else if (mode == 2){
+    modeTwo();
+  }
+}
+
+void switchToModeOne(){
+  mode = 1;
+}
+
+void switchToModeTwo(){
+  mode = 2;
 }
 
 void modeOne(){
@@ -36,10 +52,16 @@ void modeOne(){
   Servo1.write(180);
   delay(1000);
   Servo1.write(90);
+
+  mode = 0;
 }
 
 void modeTwo(){
+  Servo1.write(90);
+  delay(600);
   Servo1.write(120);
   delay(1000);
   Servo1.write(90);
+
+  mode = 0;
 }
